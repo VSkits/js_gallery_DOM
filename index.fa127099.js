0de526075a7f0535666abcfa796d45742e098665
Victor Skits <vityok.nice@gmail.com>
2024-06-20 14:47:08 +0300

    make build

---
+++ index.fa127099.js
@@ -0,0 +1,2 @@
+document.querySelector(".gallery__list").addEventListener("click",function(e){e.preventDefault();var t=e.target.closest(".list-item__link");document.getElementById("largeImg").src=t.href});
+//# sourceMappingURL=index.fa127099.js.map
